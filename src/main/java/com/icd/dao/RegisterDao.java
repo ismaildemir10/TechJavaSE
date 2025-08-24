284623dd640173d0d6444db902df793aa87285ac
iscdr <iscdr10@gmail.com>
2025-08-24 21:15:21 +0300

    base and register

---
+++ src/main/java/com/icd/dao/RegisterDao.java
@@ -0,0 +1,31 @@
+package com.icd.dao;
+
+import java.util.ArrayList;
+
+public class RegisterDao implements IDaoGeneric<RegisterDao>{
+
+    @Override
+    public RegisterDao create(RegisterDao registerDao) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<RegisterDao> list() {
+        return null;
+    }
+
+    @Override
+    public RegisterDao findById(RegisterDao registerDao) {
+        return null;
+    }
+
+    @Override
+    public RegisterDao update(RegisterDao registerDao) {
+        return null;
+    }
+
+    @Override
+    public RegisterDao delete(RegisterDao registerDao) {
+        return null;
+    }
+}
